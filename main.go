0c620d335860e9f0d42aecbfcaa7ae6dbd945cf3
Alex Bazhenov <vorobushec13@gmail.com>
2024-06-07 19:09:20 +0300

    findOriginalArray

--- main.go
+++ main.go
@@ -2,19 +2,46 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	a := "dslfr"
-	fmt.Println(checkIfPangram(a))
+	a := make([]int, 0)
+	a = append(a, 18)
+	a = append(a, 0)
+	a = append(a, 9)
+	a = append(a, 18)
+
+	fmt.Println(findOriginalArray(a))
 }
 
-func checkIfPangram(sentence string) bool {
-	mp := make(map[rune]struct{})
-	for _, v := range sentence {
-		if _, ok := mp[v]; !ok {
-			mp[v] = struct{}{}
+func findOriginalArray(changed []int) []int {
+	if len(changed)%2 != 0 {
+		return []int{}
+	}
+
+	sort.Ints(changed)
+
+	result := []int{}
+	mp := make(map[int]int)
+
+	for _, v := range changed {
+		mp[v]++
+	}
+
+	for _, v := range changed {
+
+		if mp[v] > 0 {
+			mp[v]--
+			if v, ok := mp[v*2]; ok {
+				if v > 0 {
+					result = append(result, v)
+					mp[v*2]--
+				} else {
+					return []int{}
+				}
+			}
 		}
 	}
-	return len(mp) == 26
+	return result
 }
